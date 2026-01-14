0349ac97196448b6e7f7702c87c36c638793278c
java-devel2025 <kuznya9@mail.ru>
2026-01-14 09:34:27 +0700

    работает на порту server.port=8081

--- src/main/java/org/skypro/skyshop/service/StorageService.java
+++ src/main/java/org/skypro/skyshop/service/StorageService.java
@@ -6,6 +6,7 @@ import org.skypro.skyshop.model.product.Product;
 import org.skypro.skyshop.model.product.SimpleProduct;
 import org.skypro.skyshop.model.search.Searchable;
 import org.springframework.stereotype.Service;
+import org.skypro.skyshop.exception.NoSuchProductException;
 
 import java.util.*;
 
@@ -47,8 +48,12 @@ public class StorageService {
         return result;
     }
 
-    public Optional<Product> getProductById(UUID id) {
-        return Optional.ofNullable(products.get(id));
+    public Product getProductById(UUID id) {
+        Product product = products.get(id);
+        if (product == null) {
+            throw new NoSuchProductException("Товар с id=" + id + " не найден");
+        }
+        return product;
     }
 
 }
